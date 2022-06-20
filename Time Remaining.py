58b4eb1784ee9f14609d8300b851576e81278234
Itzhak Levy <91373674+ItzhakLevy1@users.noreply.github.com>
2022-06-20 20:22:47 +0300

    Create Time Remaining.py
    Initial commit

---
+++ Time Remaining.py
@@ -0,0 +1,18 @@
+age = input("What is your current age?")
+
+age_as_int = int(age)
+
+days_remaning = age_as_int * 365
+weeks_remaning = age_as_int * 52
+months_remaning = age_as_int * 12
+
+message = f"You have {days_remaning} days, {weeks_remaning} weeks, and {days_remaning} days remaning."
+
+print(message)
+
+
+
+
+
+
+
